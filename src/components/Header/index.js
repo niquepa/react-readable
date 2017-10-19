f69954643eed3ebc18e84e8ecd7c82106b1927b8
Carlos Castillo <niquepa@gmail.com>
2017-10-18 22:47:54 -0400

    - drawer version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle } from 'react-mdc-web/lib';
+import { Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle, Drawer, DrawerHeader, DrawerHeaderContent, DrawerContent, Navigation, Icon, Tabbar, Tab } from 'react-mdc-web/lib';
+import MenuIcon from 'mdi-react/MenuIcon';
+import FilterIcon from 'mdi-react/FilterIcon';
 
 class Header extends Component {
+  state = {
+    isOpen: false,
+    activeCat: null
+  }
+
   render() {
     const { categories } = this.props;
 
     return (
-      <Toolbar fixed>
-        <ToolbarRow>
-          <ToolbarSection align="start">
-            <ToolbarTitle>Readable - a react project</ToolbarTitle>
-          </ToolbarSection>
-          { categories && categories.map(category => (
-            <ToolbarSection key={category.path} align="end">
-              <ToolbarTitle>{category.name}</ToolbarTitle>
+      <div>
+        <Toolbar fixed>
+          <ToolbarRow>
+            <ToolbarSection align="start">
+              <ToolbarTitle>
+                <MenuIcon onClick={() => { this.setState({ isOpen: !this.state.isOpen }); }} />
+              </ToolbarTitle>
             </ToolbarSection>
-            ))}
-        </ToolbarRow>
-      </Toolbar>
+            <ToolbarSection align="start">
+              <ToolbarTitle>Readable - a react project</ToolbarTitle>
+            </ToolbarSection>
+            <Tabbar>
+            { categories && categories.map(category => (
+                <Tab
+                  active={this.state.activeCat===category.path}
+                  onClick={() => {this.setState({activeCat:category.path})}}
+                >
+                  {category.name}
+                </Tab>
+              ))}
+              <span className="mdc-tab-bar__indicator"></span>
+            </Tabbar>
+          </ToolbarRow>
+        </Toolbar>
+        <Drawer
+          open={this.state.isOpen}
+          onClose={() => { this.setState({ isOpen: false }); }}
+        >
+          <DrawerHeader>
+            <DrawerHeaderContent>
+              Categories
+            </DrawerHeaderContent>
+          </DrawerHeader>
+          <DrawerContent>
+            <Navigation>
+              { categories && categories.map(category => (
+                <a key={category.path} href="#" selected><Icon />{category.name.toUpperCase()}</a>
+                ))}
+            </Navigation>
+          </DrawerContent>
+        </Drawer>
+      </div>
     );
   }
 }
