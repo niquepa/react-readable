2a309a28e56627458cd9828eb53275cabd6c6928
Carlos Castillo <niquepa@gmail.com>
2017-10-27 18:36:31 -0400

    - commentCard

---
+++ src/components/Comments/CommentCard.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { Card, CardHeader, CardTitle, CardSubtitle, CardMedia, CardText, CardActions, Button, Icon, Grid, Cell, Caption, Title } from 'react-mdc-web/lib';
+// import PostVote from './PostVote';
+import CommentVote from './CommentVote'
+import { deleteComment } from '../../actions/index';
+
+class CommentCard extends Component {
+  deleteComment = (comment) => {
+    this.props.deleteComment(comment.id);
+  }
+  //
+  // summary = (str) => {
+  //   if (str.length > 50) {
+  //     return str.substr(0, 50);
+  //   }
+  //
+  //   return str;
+  // }
+
+  render() {
+    const { comment } = this.props;
+
+    return (
+      <Cell col={4} tablet={6} phone={12} key={comment.id}>
+        <Card className="post-card" key={comment.id}>
+          <CardHeader>
+            <CardTitle>by <b>{comment.author}</b>></CardTitle>
+            <CardSubtitle>
+              Last updated: {comment.timestamp}
+            </CardSubtitle>
+            <CardSubtitle>
+              <CommentVote commentId={comment.id} voteScore={comment.voteScore} />
+            </CardSubtitle>
+          </CardHeader>
+          <CardText>
+            {comment.body}
+          </CardText>
+          <CardActions>
+            <Grid>
+              <Cell col={12}>
+                <Button raised dense primary className="card-buttons"><Icon name="edit" className="mdc-button__icon" /></Button>
+                <Button raised dense primary className="card-buttons" onClick={() => this.deleteComment(comment)}><Icon name="delete" className="mdc-button__icon" /></Button>
+              </Cell>
+            </Grid>
+          </CardActions>
+        </Card>
+      </Cell>
+    );
+  }
+}
+
+const mapStateToProps = ({ global }) => ({
+  // comments: global.comments,
+});
+
+const mapDispatchToProps = dispatch => ({
+  // fetchComments: post => dispatch(fetchComments(post)),
+  deleteComment: comment => dispatch(deleteComment(comment)),
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CommentCard));
