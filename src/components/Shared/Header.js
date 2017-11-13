56a94dc0d02ce54db83b503583da97af02719758
Carlos Castillo <ccastillo.consultant@dot.nyc.gov>
2017-11-13 00:32:52 -0500

    - header to pure component

--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,51 +1,49 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Route, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Toolbar, ToolbarRow, ToolbarSection, ToolbarTitle, Tabbar, Tab, Snackbar } from 'react-mdc-web/lib';
 import { removeSnack } from '../../actions/index';
 
-class Header extends PureComponent {
-  render() {
-    const { categories, snack } = this.props;
-    const openSnack = !!(snack && snack.length > 0);
+const Header = (props) => {
+  const { categories, snack } = props;
+  const openSnack = !!(snack && snack.length > 0);
 
-    return (
-      <div>
-        <Toolbar fixed>
-          <ToolbarRow>
-            <ToolbarSection align="start">
-              <ToolbarTitle><Link to="/" className="header-link">Readable</Link></ToolbarTitle>
-            </ToolbarSection>
-            <ToolbarSection align="end">
-              <Tabbar>
-                { categories && categories.map(category => (
-                  <Route
-                    key={category.path}
-                    path={`/${category.path}`}
-  // eslint-disable-next-line react/no-children-prop
-                    children={({ match }) => (
-                      <Tab active={!!match} key={category.path} component="span">
-                        <Link to={`/${category.path}`} className="header-link">{category.name}</Link>
-                      </Tab>
-                  )}
-                  />
-                ))}
-                <span className="mdc-tab-bar__indicator" />
-              </Tabbar>
-            </ToolbarSection>
-          </ToolbarRow>
-        </Toolbar>
-        <Snackbar
-          onTimeout={() => { this.props.removeSnack(); }}
-          open={openSnack}
-        >
-          {snack}
-        </Snackbar>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Toolbar fixed>
+        <ToolbarRow>
+          <ToolbarSection align="start">
+            <ToolbarTitle><Link to="/" className="header-link">Readable</Link></ToolbarTitle>
+          </ToolbarSection>
+          <ToolbarSection align="end">
+            <Tabbar>
+              { categories && categories.map(category => (
+                <Route
+                  key={category.path}
+                  path={`/${category.path}`}
+// eslint-disable-next-line react/no-children-prop
+                  children={({ match }) => (
+                    <Tab active={!!match} key={category.path} component="span">
+                      <Link to={`/${category.path}`} className="header-link">{category.name}</Link>
+                    </Tab>
+                )}
+                />
+              ))}
+              <span className="mdc-tab-bar__indicator" />
+            </Tabbar>
+          </ToolbarSection>
+        </ToolbarRow>
+      </Toolbar>
+      <Snackbar
+        onTimeout={() => { props.removeSnack(); }}
+        open={openSnack}
+      >
+        {snack}
+      </Snackbar>
+    </div>
+  );
+};
 
 const mapStateToProps = ({ global }) => ({
   categories: global.categories,
